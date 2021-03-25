1642474823c8a2be28e8346479efb83c3462535b
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-25 12:47:37 +0530

    Restructured Navbar without material-ui

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,57 +1,42 @@
-import React from "react";
-
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Container,
-  makeStyles
-} from "@material-ui/core";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: "#fff",
-    padding: "20px 40px",
-    color: "#000"
-  },
-  name: {
-    fontSize: "3rem",
-    fontFamily: ["Pacifico", "cursive"].join(","),
-    flexGrow: "1"
-  },
-  button: {
-    fontSize: "1.1rem",
-    marginRight: theme.spacing(2)
-  }
-}));
+import React, { useEffect } from "react";
 
 const Navbar = () => {
-  const style = useStyles();
+  useEffect(() => {
+    document.addEventListener("scroll", function () {
+      if (window.pageYOffset === 0) {
+        document.getElementById("navbar").classList.remove("scrolled");
+      } else {
+        document.getElementById("navbar").classList.add("scrolled");
+      }
+    });
+  });
+
   return (
-    <AppBar className={style.root} position="fixed">
-      <Container>
-        <Toolbar>
-          <Typography className={style.name} variant="h3">
-            <a href="#home" className={style.name}>
+    <nav id="navbar">
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col s12">
+            <a href="/" className="brand-logo">
               H K
             </a>
-          </Typography>
-          <Button href="#about" className={style.button}>
-            About
-          </Button>
-          <Button href="#skills" className={style.button}>
-            Skills
-          </Button>
-          <Button href="#works" className={style.button}>
-            Projects
-          </Button>
-          <Button href="#contact" className={style.button}>
-            Contact Me
-          </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#works">My Works</a>
+              </li>
+              <li>
+                <a href="#contact">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
