29fbe1c262f670df27d17b547787bab6743eeb35
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-30 20:35:19 +0530

    Created About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="about" className="section">
+      <div className="row">
+        <div className="col s12 center">
+          <div className="heading-primary">
+            About Me
+            <span className="line"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
