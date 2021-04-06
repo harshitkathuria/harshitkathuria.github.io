acc970b75154491ca0d3997268c74c76b4ac7a99
harshitkathuria <harshitkathuria14@gmail.com>
2021-04-06 23:29:15 +0530

    Added animation to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Skills = () => {
   return (
@@ -44,23 +45,79 @@ const Skills = () => {
         </div>
       </div> */}
 
+      {/* <div className="row center">
+        <div className="col s12">
+          <div className="heading-skills">Development</div>
+        </div>
+      </div> */}
+
       <div className="skills-container">
-        <div className="skill center">
-          <img src="/assets/JS.png" alt="" className="responsive-img" />
-          <h5>Javascript</h5>
-        </div>
-        <div className="skill center">
-          <img src="/assets/Express.png" alt="" className="responsive-img" />
-          <h5>Express</h5>
-        </div>
-        <div className="skill center">
-          <img src="/assets/React.png" alt="" className="responsive-img" />
-          <h5>React</h5>
-        </div>
-        <div className="skill center">
-          <img src="/assets/Node.png" alt="" className="responsive-img" />
-          <h5>Node</h5>
-        </div>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/JS.png" alt="" className="responsive-img" />
+            <h5>Javascript</h5>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/Express.png" alt="" className="responsive-img" />
+            <h5>Express</h5>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/React.png" alt="" className="responsive-img" />
+            <h5>React</h5>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/Node.png" alt="" className="responsive-img" />
+            <h5>Node</h5>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/MongoDB.png" alt="" className="responsive-img" />
+            <h5>MongoDB</h5>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInUp"
+          duration={1.5}
+          initiallyVisible={false}
+          animateOnce={true}
+        >
+          <div className="skill center">
+            <img src="/assets/Electron.png" alt="" className="responsive-img" />
+            <h5>Electron</h5>
+          </div>
+        </ScrollAnimation>
       </div>
     </div>
   );
