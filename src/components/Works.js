2c43864ccbad04469c1add2e8cc996dbe75a3b7a
harshitkathuria <harshitkathuria14@gmail.com>
2021-04-08 12:20:48 +0530

    Changed Works sectionand added images

--- src/components/Works.js
+++ src/components/Works.js
@@ -14,193 +14,236 @@ const Works = () => {
       <div id="projects">
         <div className="project row">
           <div className="col s12">
-            <div className="project-image col s6 m5">
-              <img
-                src="/assets/projects/foodtime.jpg"
-                alt=""
-                className="responsive-img"
-              />
-            </div>
-            <div className="project-about col s6 m7 center">
-              <div className="text">
-                <h2 className="heading-about">FoodTime</h2>
-                <div className="content">
-                  <p>
-                    A Food-Ordering Application built for both Consumers and
-                    Restaurants.
-                  </p>
-                  <br />
-                  <p>
-                    Restaurants can post their outlets with various dishes, and
-                    Consumers can place order from restaurant by adding dishes
-                    of specific quantity.
-                  </p>
-                  <br />
-                  <p>
-                    Built with <strong>MERN</strong> Stack
-                  </p>
+            <div
+              className="card horizontal"
+              style={{ backgroundColor: "inherit" }}
+            >
+              <div className="card-image left valign-wrapper">
+                <img
+                  src="/assets/projects/foodtime.jpg"
+                  alt=""
+                  className="responsive-img"
+                />
+              </div>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <div className="project-about">
+                    <div className="text">
+                      <h2 className="heading-about">FoodTime</h2>
+                      <div className="content">
+                        <p>
+                          A Food-Ordering Application built for both Consumers
+                          and Restaurants.
+                        </p>
+                        <br />
+                        <p>
+                          Restaurants can post their outlets with various
+                          dishes, and Consumers can place order from restaurant
+                          by adding dishes of specific quantity.
+                        </p>
+                        <br />
+                        <p>
+                          Built with <strong>MERN</strong> Stack
+                        </p>
+                      </div>
+                      <div className="card-action">
+                        <p className="left">
+                          <a
+                            href="https://foodtime-app.herokuapp.com"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            View Project
+                          </a>
+                        </p>
+                        <p className="right">
+                          <a
+                            href="https://www.github.com/harshitkathuria/foodtime"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Source Code
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="source-links">
-                  <p className="left">
-                    <a
-                      href="https://foodtime-app.herokuapp.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      View Project
-                    </a>
-                  </p>
-                  <p className="right">
-                    <a
-                      href="https://www.github.com/harshitkathuria/foodtime"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Source Code
-                    </a>
-                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="project row">
+          <div className="col s12">
+            <div
+              className="card horizontal"
+              style={{ backgroundColor: "inherit" }}
+            >
+              <div className="card-image left valign-wrapper">
+                <img
+                  src="/assets/projects/interndigger.jpg"
+                  alt=""
+                  className="responsive-img"
+                />
+              </div>
+              <div className="card-content">
+                <div className="project-about">
+                  <div className="text">
+                    <h2 className="heading-about">InternDigger</h2>
+                    <div className="content">
+                      <p>
+                        An Internship portal built for student and recruiters
+                        for providing a platform for an easy hiring.
+                      </p>
+                      <br />
+                      <p>
+                        Recruiters can post their job openings with different
+                        roles and other fields, and Students can apply for them.
+                        They can also filter the opening by adding various
+                        filters.
+                      </p>
+                    </div>
+                    <div className="card-action">
+                      <p className="left">
+                        <a
+                          href="https://interndigger.herokuapp.com/"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          View Project
+                        </a>
+                      </p>
+                      <p className="right">
+                        <a
+                          href="https://www.github.com/harshitkathuria/interndigger"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Source Code
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="project row">
-          <div className="project-image col s6 m5">
-            <img
-              src="/assets/projects/interndigger.jpg"
-              alt=""
-              className="responsive-img"
-            />
-          </div>
-          <div className="project-about col s6 m7 center">
-            <div className="text">
-              <h2 className="heading-about">InternDigger</h2>
-              <div className="content">
-                <p>
-                  An Internship portal built for student and recruiters for
-                  providing a platform for an easy hiring.
-                </p>
-                <br />
-                <p>
-                  Recruiters can post their job openings with different roles
-                  and other fields, and Students can apply for them. They can
-                  also filter the opening by adding various filters.
-                </p>
+          <div className="col s12">
+            <div
+              className="card horizontal"
+              style={{ backgroundColor: "inherit" }}
+            >
+              <div className="card-image left valign-wrapper">
+                <img
+                  src="/assets/projects/chatout.png"
+                  alt=""
+                  className="responsive-img"
+                />
               </div>
-              <div className="source-links">
-                <p className="left">
-                  <a
-                    href="https://interndigger.herokuapp.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Project
-                  </a>
-                </p>
-                <p className="right">
-                  <a
-                    href="https://www.github.com/harshitkathuria/interndigger"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Source Code
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="project row">
-          <div className="project-image col s6 m5">
-            <img
-              src="/assets/projects/chatout.png"
-              alt=""
-              className="responsive-img"
-            />
-          </div>
-          <div className="project-about col s6 m7 center">
-            <div className="text">
-              <h2 className="heading-about">ChatOut</h2>
-              <div className="content">
-                <p>
-                  A Real Time Web Chat Application made for people who love
-                  chatting without any hassle.
-                </p>
-                <br />
-                <p>
-                  Users can join anyone of the various rooms. Can be used as
-                  both <strong>Web </strong> and <strong>Desktop</strong>{" "}
-                  application. Connected to MongoDB database, so that the users
-                  can view the previous chats also.
-                </p>
-                <br />
-                <p>
-                  This was my first introduction with{" "}
-                  <strong>socket.io. </strong>
-                  Integrated electronjs to use it as a{" "}
-                  <strong>Desktop Client</strong> also.
-                </p>
-              </div>
-              <div className="source-links">
-                <p className="left">
-                  <a
-                    href="https://chatout-live.herokuapp.com"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Project
-                  </a>
-                </p>
-                <p className="right">
-                  <a
-                    href="https://www.github.com/harshitkathuria/chatout"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Source Code
-                  </a>
-                </p>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <div className="project-about">
+                    <div className="text">
+                      <h2 className="heading-about">ChatOut</h2>
+                      <div className="content">
+                        <p>
+                          A Real Time Web Chat Application made for people who
+                          love chatting without any hassle.
+                        </p>
+                        <br />
+                        <p>
+                          Users can join anyone of the various rooms. Can be
+                          used as both <strong>Web </strong> and{" "}
+                          <strong>Desktop</strong> application. Connected to
+                          MongoDB database, so that the users can view the
+                          previous chats also.
+                        </p>
+                        <br />
+                        <p>
+                          This was my first introduction with{" "}
+                          <strong>socket.io. </strong>
+                          Integrated electronjs to use it as a{" "}
+                          <strong>Desktop Client</strong> also.
+                        </p>
+                      </div>
+                      <div className="card-action">
+                        <p className="left">
+                          <a
+                            href="https://chatout-live.herokuapp.com"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            View Project
+                          </a>
+                        </p>
+                        <p className="right">
+                          <a
+                            href="https://www.github.com/harshitkathuria/chatout"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Source Code
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="project row">
-          <div className="project-image col s6 m5">
-            <img
-              src="/assets/projects/dynamoplayer.png"
-              alt=""
-              className="responsive-img"
-            />
-          </div>
-          <div className="project-about col s6 m7 center">
-            <div className="text">
-              <h2 className="heading-about">DynamoPlayer</h2>
-              <div className="content">
-                <p>
-                  A Cross - Platform Music Player built with electronjs for
-                  people who love listening music.
-                </p>
-                <br />
-                <p>
-                  Users can load songs from their system and add them to the
-                  queue. They can select song in any order from the queue. Songs
-                  list can be hided.
-                </p>
-                <br />
-                <p>
-                  This was my first introduction to <strong>electronjs</strong>.
-                </p>
+          <div className="col s12">
+            <div
+              className="card horizontal"
+              style={{ backgroundColor: "inherit" }}
+            >
+              <div className="card-image left valign-wrapper">
+                <img
+                  src="/assets/projects/dynamoplayer.png"
+                  alt=""
+                  className="responsive-img"
+                />
               </div>
-              <div className="source-links">
-                <p>
-                  <a
-                    href="https://www.github.com/harshitkathuria/dynamoplayer"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Source Code
-                  </a>
-                </p>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <div className="project-about">
+                    <div className="text">
+                      <h2 className="heading-about">DynamoPlayer</h2>
+                      <div className="content">
+                        <p>
+                          A Cross - Platform Music Player built with electronjs
+                          for people who love listening music.
+                        </p>
+                        <br />
+                        <p>
+                          Users can load songs from their system and add them to
+                          the queue. They can select song in any order from the
+                          queue. Songs list can be hided.
+                        </p>
+                        <br />
+                        <p>
+                          This was my first introduction to{" "}
+                          <strong>electronjs</strong>.
+                        </p>
+                      </div>
+                      <div className="card-action">
+                        <p>
+                          <a
+                            href="https://www.github.com/harshitkathuria/dynamoplayer"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Source Code
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
